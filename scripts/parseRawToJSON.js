fd71eefc15735f98ed88fb7bac12b125f6fb0bbf
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2022-08-19 00:59:53 -0400

    better data pre cleaning

--- scripts/parseRawToJSON.js
+++ scripts/parseRawToJSON.js
@@ -26,12 +26,55 @@ const camelCase = (str) => {
     .replace("\r", "");
 };
 
+//takes an array of an array which gets rid of empty cells and splits apart cells with multiple values (split by a space)
+const moosh = (arr) => {
+  arr = JSON.parse(JSON.stringify(arr)); //stupid deep copy
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let row = arr[i];
+    let newRow = [];
+
+    //skipping row if it is mostly empty
+    let countNotEmpty = 0;
+    row.forEach((cell) => {
+      if (cell !== "") countNotEmpty++;
+    });
+
+    if (countNotEmpty > row.length / 4) {
+      for (let j = 0; j < row.length; j++) {
+        if (row[j].includes(" ")) {
+          row.splice(j, 1, ...row[j].split(" "));
+        }
+
+        if (row[j] === "N/A" || row[j] === "-") {
+          row[j] = "0";
+        }
+
+        if (isNaN(parseNum(row[j])) || row[j] === "" || row[j] === "$") {
+          row.splice(j, 1);
+          j--;
+          continue;
+        }
+
+        newRow.push(parseNum(row[j]));
+      }
+    }
+
+    newArr.push(newRow);
+  }
+
+  return newArr;
+};
+
 const processFile = (fileName) => {
   const rawData = fs.readFileSync(fileName, "utf8");
   const data = parse(rawData, {
     columns: false,
     skip_empty_lines: true,
   });
+
+  const mooshed = moosh(data);
 
   const fileType = fileName.split("tabula-report-")[1].split(".")[0];
 
@@ -45,18 +88,27 @@ const processFile = (fileName) => {
       data.shift();
       data.shift();
 
-      data.forEach((row) => {
+      mooshed.shift();
+      mooshed.shift();
+      mooshed.shift();
+
+      mooshed.forEach((row, i) => {
+        console.log(row);
+        console.log(data[i]);
+
+        if (row.length == 0) return;
+
         const parsed = {
-          priorYear: parseNum(row[2]),
-          currentYearForecast: parseNum(row[9]),
-          currentYearActual: parseNum(row[4]),
-          growthInDollars: parseNum(row[5] ? row[5] : row[6]),
-          growthInPercent: parseNum(row[7]),
-          actualVSForecastInDollars: parseNum(row[10].split(" ")[1]),
-          actualVSForecastInPercent: parseNum(row[10].split(" ")[2]),
+          priorYear: row[0] * 1000000,
+          currentYearForecast: row[4] * 1000000,
+          currentYearActual: row[1] * 1000000,
+          growthInDollars: row[2] * 1000000,
+          growthInPercent: row[3],
+          actualVSForecastInDollars: row[6] * 1000000,
+          actualVSForecastInPercent: row[7],
         };
 
-        final[camelCase(row[0])] = parsed;
+        final[camelCase(data[i][0])] = parsed;
       });
       break;
     case "1":
@@ -68,12 +120,12 @@ const processFile = (fileName) => {
 
       data.forEach((row) => {
         const parsed = {
-          priorYear: parseNum(row[1]),
-          currentYearForecast: parseNum(row[5]),
-          currentYearActual: parseNum(row[2]),
-          growthInDollars: parseNum(row[4].split(" ")[0]),
+          priorYear: parseNum(row[1]) * 1000000,
+          currentYearForecast: parseNum(row[5]) * 1000000,
+          currentYearActual: parseNum(row[2]) * 1000000,
+          growthInDollars: parseNum(row[4].split(" ")[0]) * 1000000,
           growthInPercent: parseNum(row[4].split(" ")[1]),
-          actualVSForecastInDollars: parseNum(row[6].split(" ")[1]),
+          actualVSForecastInDollars: parseNum(row[6].split(" ")[1]) * 1000000,
           actualVSForecastInPercent: parseNum(row[6].split(" ")[2]),
         };
 
@@ -94,12 +146,12 @@ const processFile = (fileName) => {
         }
 
         const parsed = {
-          priorYear: parseNum(row[1]),
-          currentYearForecast: parseNum(row[2]),
-          currentYearActual: parseNum(row[3]),
-          growthInDollars: parseNum(row[5]),
+          priorYear: parseNum(row[1]) * 1000,
+          currentYearForecast: parseNum(row[2]) * 1000,
+          currentYearActual: parseNum(row[3]) * 1000,
+          growthInNumbers: parseNum(row[5]) * 1000,
           growthInPercent: parseNum(row[6]),
-          actualVSForecastInDollars: parseNum(row[4].split(" ")[0]),
+          actualVSForecastInNumbers: parseNum(row[4].split(" ")[0]),
           actualVSForecastInPercent:
             row[4].split(" ")[1] != "pp"
               ? parseNum(row[4].split(" ")[1])
@@ -147,11 +199,13 @@ const processFile = (fileName) => {
           operatingRevenue: parseNum(row[1]) * 1000000,
           operatingExpenses: parseNum(row[3]) * 1000000,
           operatingEarnings: parseNum(row[5]) * 1000000,
-          ridership: row[6] !== '' ? parseNum(row[6]) : 0,
-          seatMiles: row[7] !== '' ? parseNum(row[7].split(" ")[0]) : 0,
-          passengerMiles: row[7] !== '' ? parseNum(row[7].split(" ")[1]) : 0,
-          averageLoadFactor: row[8] !== '' ? parseNum(row[8]) : 0,
-          onTimePerformance: row[9] !== '' ? parseNum(row[9]) : 0,
+          ridership: row[6] !== "" ? parseNum(row[6]) * 1000 : 0,
+          seatMiles:
+            row[7] !== "" ? parseNum(row[7].split(" ")[0]) * 1000000 : 0,
+          passengerMiles:
+            row[7] !== "" ? parseNum(row[7].split(" ")[1]) * 1000000 : 0,
+          averageLoadFactor: row[8] !== "" ? parseNum(row[8]) : 0,
+          onTimePerformance: row[9] !== "" ? parseNum(row[9]) : 0,
           trainMiles: parseNum(row[10]) * 1000000,
           frequencies: parseNum(row[11]),
         };
@@ -168,11 +222,11 @@ const processFile = (fileName) => {
           parsed.seatMiles +=
             data[i - 1][7].split(" ")[0] === "N/A"
               ? 0
-              : parseNum(data[i - 1][7].split(" ")[0]);
+              : parseNum(data[i - 1][7].split(" ")[0]) * 1000000;
           parsed.passengerMiles +=
             data[i - 1][7].split(" ")[1] === "N/A"
               ? 0
-              : parseNum(data[i - 1][7].split(" ")[1]);
+              : parseNum(data[i - 1][7].split(" ")[1]) * 1000000;
           parsed.averageLoadFactor +=
             data[i - 1][8] === "N/A" ? 0 : parseNum(data[i - 1][8]);
           parsed.onTimePerformance +=
@@ -199,6 +253,7 @@ const processFile = (fileName) => {
 const readFolder = (folderPath) => {
   return fs.readdirSync(folderPath).map((file) => {
     const filePath = path.join(folderPath, file);
+    console.log("processing file ", filePath);
     const data = processFile(filePath);
     return data;
   });
@@ -220,6 +275,21 @@ const readYear = (year) => {
   return months;
 };
 
+/*
 console.log(
-  processFile("../reports/2021/september/unprocessed/tabula-report-4.csv")
+  readMonth("../reports/2021/august")
 );
+*/
+
+console.log(
+  processFile("../reports/2021/august/unprocessed/tabula-report-0.csv")
+);
+
+/*
+const rawData = fs.readFileSync(
+  "../reports/2021/september/unprocessed/tabula-report-4.csv",
+  "utf8"
+);
+const data = parse(rawData, { columns: false, skip_empty_lines: true });
+console.log(moosh(data));
+*/
